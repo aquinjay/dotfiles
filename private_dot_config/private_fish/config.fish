25b947bc8512f5cd0f5188df62ba28ccb0cd466d
aquinjay <aquinteroj@pm.me>
2022-10-15 11:15:42 -0700

    Initial commit

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/main/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+export PATH="$HOME/../../bin/nvim-linux64/bin:$PATH"
+export PATH="$HOME/../../bin/bin/:$PATH"
+
