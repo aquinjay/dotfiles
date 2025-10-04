49ed80fd019bdd0c8934bd10ebcc7552fb95a97a
aquinjay <103866978+aquinjay@users.noreply.github.com>
2025-10-04 12:58:21 -0700

    Configure hardtime.nvim behavior

--- private_dot_config/nvim/lua/user/plugins/hardtime.lua
+++ private_dot_config/nvim/lua/user/plugins/hardtime.lua
@@ -1,8 +1,30 @@
 local M = {
-   "m4xshen/hardtime.nvim",
-   dependencies = { "MunifTanjim/nui.nvim" },
-   event = "VeryLazy",
-   opts = {}
+  "m4xshen/hardtime.nvim",
+  dependencies = { "MunifTanjim/nui.nvim" },
+  event = "VeryLazy",
+  -- Encourage practicing efficient navigation while avoiding disruption in special buffers.
+  opts = {
+    disable_mouse = false,
+    disabled_filetypes = {
+      "neo-tree",
+      "lazy",
+      "mason",
+      "help",
+      "qf",
+      "TelescopePrompt",
+    },
+    restriction_mode = "hint",
+    restricted_keys = {
+      ["h"] = { "n", "x" },
+      ["j"] = { "n", "x" },
+      ["k"] = { "n", "x" },
+      ["l"] = { "n", "x" },
+      ["-"] = { "n", "x" },
+      ["+"] = { "n", "x" },
+      ["gj"] = { "n", "x" },
+      ["gk"] = { "n", "x" },
+    },
+  },
 }
 
 return M
