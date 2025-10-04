569ee48797c42a641e013a617deb40612078f8d9
aquinjay <103866978+aquinjay@users.noreply.github.com>
2025-10-04 16:18:28 -0700

    Set default Nerd Font for Nerdy picker

--- private_dot_config/nvim/lua/user/plugins/nerdy.lua
+++ private_dot_config/nvim/lua/user/plugins/nerdy.lua
@@ -1,10 +1,31 @@
+---@type LazySpec
 local M = {
+  -- Plugin providing Telescope picker to browse Nerd Font icons.
   "2KAbhishek/nerdy.nvim",
   dependencies = {
-    'stevearc/dressing.nvim',
-  'nvim-telescope/telescope.nvim'
+    -- Fancy UI enhancements for picker prompts.
+    "stevearc/dressing.nvim",
+    -- Nerdy piggybacks on Telescope for its picker UI.
+    "nvim-telescope/telescope.nvim",
   },
-  cmd = 'Nerdy'
+  -- Load plugin only for the :Nerdy command or defined keymaps.
+  cmd = "Nerdy",
+  keys = {
+    {
+      "<leader>fn",
+      "<cmd>Nerdy<CR>",
+      desc = "Find Nerd Font icon",
+    },
+  },
+  config = function()
+    -- Fall back to JetBrainsMono if no user preference is provided.
+    local preferred_font = vim.g.preferred_nerd_font or vim.env.NERD_FONT or "JetBrainsMono Nerd Font"
+
+    require("nerdy").setup {
+      -- Limit picker entries to a single Nerd Font family for consistent glyphs.
+      nerd_font = preferred_font,
+    }
+  end,
 }
 
 return M
