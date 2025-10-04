468a13e34bb8c880ead5647907a21e1ea0efbd3d
aquinjay <103866978+aquinjay@users.noreply.github.com>
2025-10-04 12:55:26 -0700

    Document devicons plugin configuration

--- private_dot_config/nvim/lua/user/plugins/devicons.lua
+++ private_dot_config/nvim/lua/user/plugins/devicons.lua
@@ -1,9 +1,13 @@
+-- Configure UI file-type icons provided by nvim-web-devicons.
+-- The plugin is lazy-loaded on the VeryLazy event to avoid slowing down startup.
 local M = {
   "nvim-tree/nvim-web-devicons",
   event = "VeryLazy",
 }
 
 function M.config()
+  -- Use the default setup for devicons. This provides icons for filetypes in
+  -- statuslines, completion menus, and other UI components that support them.
   require('nvim-web-devicons').setup{}
 end
 
